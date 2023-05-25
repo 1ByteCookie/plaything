7726f36e1c040b5f75dd0f283eefd9ad18c979da
Ian Red <68849067+1ByteCookie@users.noreply.github.com>
2023-05-25 16:06:46 +0800

    sdl_image

--- plaything/Scripts/build.py
+++ plaything/Scripts/build.py
@@ -6,3 +6,4 @@ SolutionDir = sys.argv[1]
 OutDir = sys.argv[2]
 
 shutil.copy(SolutionDir + "Dependencies\SDL2\lib\SDL2.dll", OutDir)
+shutil.copy(SolutionDir + "Dependencies\SDL2\lib\SDL2_image.dll", OutDir)
